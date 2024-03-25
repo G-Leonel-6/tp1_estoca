440c5343bf41bc0cfd3751173669a1717805cfe4
NehUcce <33404336+NehUcce@users.noreply.github.com>
2024-03-25 10:36:19 -0300

    Update trabajo_practico_1.m
    0s en el inicio para chequearlos más fácil
    + Segmentación de señal por bloques
    + Comentarios de funcionamiento

--- ej1/trabajo_practico_1.m
+++ ej1/trabajo_practico_1.m
@@ -4,19 +4,28 @@ clear all
 
 [data, fs] = audioread('audio_01_2024a.wav');
 data_norm = data / (rms(data));
+data_norm = transpose(data_norm);
+% adquisicion de recurso, normalizacion y
+% transpone para trabajar con filas
 
 L = 2;
 
+while mod(length(data_norm), L) != 0
+  data_norm = [0 data_norm];
+endwhile
+
 columnas = length(data_norm) / L;
 
-x_m = zeros(L, floor(columnas));
+% añade 0s al inicio de la señal hasta que
+% sea divisible por L y se define el ancho
+% de los vectores
 
+for i = 1:L
+  x_m(i,:) = data_norm((i-1)*columnas+1:i*columnas);
+endfor
 
-for i=1:ceil(columnas)
-    for j = 1:L
-        x_m(j, i) = data_norm(i+j-1);
-    end
-end
+% toma bloques de la señal usando el tamaño que
+% definimos y los ubica en las filas del vector aleatorio
 
 
 % b) grafico de dispersion
