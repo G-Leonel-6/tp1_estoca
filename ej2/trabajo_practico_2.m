58cd0ccba5ab17ede1c23ac585885d79c381d7ed
NehUcce <33404336+NehUcce@users.noreply.github.com>
2024-03-27 13:54:13 -0300

    Create trabajo_practico_2.m
    Incompleto y con errores
    
    Calcula cantidad de elementos para un CR dado, y usa los avas y avec para armar dos matrices con la cantidad calculada con ellos

---
+++ ej2/trabajo_practico_2.m
@@ -0,0 +1,75 @@
+clear all
+clc
+
+pkg load optim
+pkg load miscellaneous
+pkg load statistics
+pkg load signal
+
+
+%ejercicio 1
+% a) segmentacion del audio
+
+[data, fs] = audioread('audio_02_2024a.wav');
+data_norm = data / (rms(data));
+
+data_norm = transpose(data_norm);
+
+L = 1323;
+j = 0; %necesario si queremos dividir los 0
+
+while mod(length(data_norm), L) != 0
+  j += 1;
+  if mod(j,2) != 0
+    data_norm = [0 data_norm];
+  else
+   data_norm = [data_norm 0];
+   endif
+endwhile
+
+columnas = length(data_norm) / L;
+
+for i = 1:L
+  x_m(i,:) = data_norm((i-1)*columnas+1:i*columnas);
+endfor
+
+
+% c) matriz de covarianza 
+
+%matriz de covarianzas
+Cx = cov(x_m');
+
+%autovalores y autovectores
+
+[avec, avas] = eig(Cx);
+
+%Definicion de matriz Y
+
+y_m = avec' * x_m;
+
+Cy = cov(y_m');
+
+%% por alguna razon, cada fila 
+%tiene un numero que se repite en toda la fila
+
+CR = 70;
+
+K = floor((1 - (CR/100))*L); %cantidad de maximos deseados
+
+%K = 10; %K de prueba
+
+for i = 1:length(avas(1,:))
+  avaVector(i) = avas(i,i);
+endfor
+
+for i = 1:K
+  [avaMax(i),indMax(i)] = max(avaVector);
+  avaVector(indMax(i)) = 0;
+  matrizKAsoc(i,:) = avec(indMax(i),:);
+  matrizK(i,i) = avaMax(i);
+endfor
+%Obtengo los indices de los K maximos con ellos armo
+%la matriz diagonal K, cuyos elementos de la diagonal
+%son los avas de Cx ordenados de mayor a menor y
+%una matriz asociada a K, cuyos vectores columnas
+%son los avec asociados a cada ava de K (en orden)
